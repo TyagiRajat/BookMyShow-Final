d539f5e60b5a6ec4e89979144ef3c918678c2027
Rajat Tyagi <140008327+TyagiRajat@users.noreply.github.com>
2024-07-18 22:10:20 +0530

    Update App.js

--- project/client/src/App.js
+++ project/client/src/App.js
@@ -1,21 +1,21 @@
-import "./App.css";
+import "./App.css"
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import ProtectedRoute from "./components/ProtectedRoute";
-import { useSelector } from "react-redux";
-import Profile from "./pages/Profile";
-import Admin from "./pages/Admin";
-import BookShow from "./pages/BookShow";
-import SingleMovie from "./pages/SingleMovie";
-import Partner from "./pages/Partner";
-import Forget from "./pages/Forget";
-import Reset from "./pages/Reset";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import ProtectedRoute from "./components/ProtectedRoute"
+import { useSelector } from "react-redux"
+import Profile from "./pages/Profile"
+import Admin from "./pages/Admin"
+import BookShow from "./pages/BookShow"
+import SingleMovie from "./pages/SingleMovie"
+import Partner from "./pages/Partner"
+import Forget from "./pages/Forget"
+import Reset from "./pages/Reset"
 
 function App() {
-  const { loading } = useSelector((state) => state.loader);
+  const { loading } = useSelector((state) => state.loader)
 
   return (
     <div className="App">
@@ -61,14 +61,28 @@ function App() {
           />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path='/forget' element={<Forget/>}/>
-          <Route path='/reset' element={<Reset/>}/>
-          <Route path="/movie/:id" element={<ProtectedRoute><SingleMovie/></ProtectedRoute>} />
-          <Route path="/book-show/:id" element={<ProtectedRoute><BookShow/></ProtectedRoute>} />
+          <Route path="/forget" element={<Forget />} />
+          <Route path="/reset" element={<Reset />} />
+          <Route
+            path="/movie/:id"
+            element={
+              <ProtectedRoute>
+                <SingleMovie />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/book-show/:id"
+            element={
+              <ProtectedRoute>
+                <BookShow />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
