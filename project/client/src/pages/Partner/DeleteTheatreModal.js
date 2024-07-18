6abb952b8e3dfcac57a54c0a3ef2b4bf2fc59979
Rajat Tyagi <140008327+TyagiRajat@users.noreply.github.com>
2024-07-18 22:25:41 +0530

    Update DeleteTheatreModal.js

--- project/client/src/pages/Partner/DeleteTheatreModal.js
+++ project/client/src/pages/Partner/DeleteTheatreModal.js
@@ -1,46 +1,59 @@
-import { Modal, message } from 'antd';
-import { deleteMovie } from '../../calls/movies'
-import { showLoading, hideLoading } from '../../redux/loaderSlice';
-import { useDispatch } from 'react-redux';
+import { Modal, message } from "antd";
+import { deleteTheatre } from "../../calls/theatres";
+import { showLoading, hideLoading } from "../../redux/loaderSlice";
+import { useDispatch } from "react-redux";
 
-const DeleteMovieModal = ({isDeleteModalOpen, setIsDeleteModalOpen, selectedMovie, setSelectedMovie, getData}) => {
-    const dispatch =  useDispatch()
-    const handleOk = async () => {
-        try{
-            dispatch(showLoading);
-            const movieId = selectedMovie._id;
-            const response = await deleteMovie({ movieId });
-          
-            if(response.success){
-              console.log("id and respone ->  " , movieId, response);
-                message.success(response.message);
-                getData();
-            }else{
-                message.error(response.message);
-                setSelectedMovie(null);
-            }
-            setIsDeleteModalOpen(false);
-            dispatch(hideLoading);
-            
-        }catch(err){
-            dispatch(hideLoading);
-            setIsDeleteModalOpen(false)
-            message.error(err.messagae);
-        }        
-    };
+const DeleteTheatreModal = ({
+  isDeleteModalOpen,
+  setIsDeleteModalOpen,
+  selectedTheatre,
+  setSelectedTheatre,
+  getData,
+}) => {
+  const dispatch = useDispatch();
+  const handleOk = async () => {
+    try {
+      dispatch(showLoading);
+      const theatreId = selectedTheatre._id;
+      const response = await deleteTheatre({ theatreId });
+      console.log(theatreId, response);
+      if (response.success) {
+        message.success(response.message);
+        getData();
+      } else {
+        message.error(response.message);
+        setSelectedTheatre(null);
+      }
+      setIsDeleteModalOpen(false);
+      dispatch(hideLoading);
+    } catch (err) {
+      dispatch(hideLoading);
+      setIsDeleteModalOpen(false);
+      message.error(err.messagae);
+    }
+  };
   const handleCancel = () => {
     setIsDeleteModalOpen(false);
-    setSelectedMovie(null);
+    setSelectedTheatre(null);
   };
 
   return (
     <>
-      <Modal title="Delete Movie?" open={isDeleteModalOpen} onOk={handleOk} onCancel={handleCancel}>
-            <p className='pt-3 fs-18'>Are you sure you want to delete this movie?</p>
-            <p className='pb-3 fs-18'>This action can't be undone and you'll lose this movie data.</p>
+      <Modal
+        title="Delete Theatre?"
+        open={isDeleteModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <p className="pt-3 fs-18">
+          Are you sure you want to delete this theatre?
+        </p>
+        <p className="pb-3 fs-18">
+          This action can't be undone and you'll lose this theatre data.
+        </p>
       </Modal>
     </>
   );
 };
 
-export default DeleteMovieModal;
+export default DeleteTheatreModal;
